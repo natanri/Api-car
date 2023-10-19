40c0f54f548211a544bed2dd6947cd8b83841bfa
angelaslinker <angela.r.slinker@gmail.com>
2023-10-18 17:51:13 -0700

    debugging

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-const passport = require('passport');
 const router = require('express').Router();
+const passport = require('passport');
 router.use('/', require('./swagger'));
 
 router.get('/', (req, res) => {
