e9fd8aba9003fbd5185746287db0b235922e4d93
Jamest011 <ttysjr@gmail.com>
2023-10-23 20:03:50 -0500

    more testing

--- helpers/validate.js
+++ helpers/validate.js
@@ -1,8 +1,36 @@
-const validator = require('express-validator');
-const validation = (body, rules, customMessages, callback) =>{
-    const validation = new ValidatorsImpl(body, rules, customMessages, callback);
-    validation.passes(() => callback(null, true));
-    validation.fails(() => callback(validation.errors, false));
+// const validator = require('express-validator');
+// const validation = (body, rules, customMessages, callback) =>{
+//     const validation = new ValidatorsImpl(body, rules, customMessages, callback);
+//     validation.passes(() => callback(null, true));
+//     validation.fails(() => callback(validation.errors, false));
+// };
+
+// module.exports = validator;
+
+const { validationResult, body } = require('express-validator');
+
+const validate = (validationRules) => {
+    return [
+        // Define your validation rules here using the 'body' method
+        // For example:
+        body('brand').notEmpty().isString(),
+        body('model').notEmpty().isString(),
+        body('year').notEmpty().isInt(),
+        body('doors').notEmpty().isInt(),
+        
+        // Handle validation errors
+        (req, res, next) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(412).json({
+                    success: false,
+                    message: 'Validation failed',
+                    errors: errors.array(),
+                });
+            }
+            next();
+        }
+    ];
 };
 
-module.exports = validator;
+module.exports = validate;
--- middleware/validate.js
+++ middleware/validate.js
@@ -1,24 +1,43 @@
-const validator = require('../helpers/validate');
+// const validator = require('../helpers/validate');
+
+// const saveCar = (req, res, next) => {
+//     const validationRule = {
+//         "brand": 'required|string',
+//         "model": 'required|string',
+//         "year": 'required|numeric',
+//         "doors": 'required|numeric'
+        
+//     };
+//     validator(req.body, validationRule, {}, (err, status) => {
+//         if (!status) {
+//             res.status(412).send({
+//                 success: false,
+//                 message: 'Validation failed',
+//                 data: err                
+//             });
+//         } else {
+//             next();
+//         }
+//     });
+// };
+
+// module.exports = {
+//     saveCar
+// };
+
+const validate = require('../helpers/validate');
 
 const saveCar = (req, res, next) => {
-    const validationRule = {
-        "brand": 'required|string',
-        "model": 'required|string',
-        "year": 'required|numeric',
-        "doors": 'required|numeric'
-        
-    };
-    validator(req.body, validationRule, {}, (err, status) => {
-        if (!status) {
-            res.status(412).send({
-                success: false,
-                message: 'Validation failed',
-                data: err                
-            });
-        } else {
-            next();
-        }
-    });
+    // Define your validation rules
+    const validationRules = [
+        body('brand').notEmpty().isString(),
+        body('model').notEmpty().isString(),
+        body('year').notEmpty().isInt(),
+        body('doors').notEmpty().isInt(),
+    ];
+
+    // Use the 'validate' function to check for validation errors
+    validate(validationRules)(req, res, next);
 };
 
 module.exports = {
