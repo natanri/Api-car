f27b7672cb86583b1cc3bf266fe86b16974d6ea2
Jamest011 <ttysjr@gmail.com>
2023-10-23 21:25:24 -0500

    updated validate files

--- helpers/validate.js
+++ helpers/validate.js
@@ -1,12 +1,3 @@
-// const validator = require('express-validator');
-// const validation = (body, rules, customMessages, callback) =>{
-//     const validation = new ValidatorsImpl(body, rules, customMessages, callback);
-//     validation.passes(() => callback(null, true));
-//     validation.fails(() => callback(validation.errors, false));
-// };
-
-// module.exports = validator;
-
 const Validator = require('validatorjs');
 const validator = (body, rules, customMessages, callback) => {
     const validation = new Validator(body, rules, customMessages, callback);
--- middleware/validate.js
+++ middleware/validate.js
@@ -28,18 +28,6 @@ const saveCar = async (req, res, next) => {
       });
     }
 };
-//     validator(req.body, validationRule, {}, (err, status) => {
-//         if (!status) {
-//             res.status(412).send({
-//                 success: false,
-//                 message: 'Validation failed',
-//                 data: err                
-//             });
-//         } else {
-//             next();
-//         }
-//     });
-// };
 
 
 module.exports = {
